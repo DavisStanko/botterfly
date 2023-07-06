3a69b7dda41f660e48f03265621b50c1a9b392d4
davis stanko <davisbvstanko@gmail.com>
2023-07-06 17:29:59 -0400

    list commands

---
+++ commands/utilities/help.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Replies with a list of commands!'),
+	async execute(interaction) {
+		// Get the commands
+        const commands = interaction.client.commands;
+        // Create an array of command names
+        const commandNames = commands.map(command => command.data.name);
+        // Create a string with the command names
+        const commandList = commandNames.join('\n');
+        // Reply with the command list
+        await interaction.reply(`Available commands:\n${commandList}`);
+	},
+};
