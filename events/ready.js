606921467e6be5e94b393235a0456a0463cdd26c
davis stanko <davisbvstanko@gmail.com>
2023-07-06 17:29:47 -0400

    log guilds and ids

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,11 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+        // get connected guilds and ids
+        const guilds = client.guilds.cache.map(guild => guild.name);
+        const guildIds = client.guilds.cache.map(guild => guild.id);
+        const guildsAndIds = guilds.map((guild, index) => `${guild} (${guildIds[index]})`);
+        // log connected guilds and ids
+        console.log(`Connected to: ${guildsAndIds.join(', ')}`);
+    },
 };
