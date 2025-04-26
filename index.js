9ba5a8bfdbb30a1a7bab5c614a94a1e67cc4a36c
davis stanko <davisbvstanko@gmail.com>
2025-04-25 22:55:13 -0400

    cors not needed as a reverse proxy is used instead

--- index.js
+++ index.js
@@ -50,11 +50,8 @@ for (const file of eventFiles) {
 
 // Express server setup
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const PORT = 3001;
-
-app.use(cors());
 
 let botStats = { guilds: 0, users: 0 };
 
